ac294ffc2b11ca804806520447500696ca75fa94
HasnatBajwa <hasnatjbajwa@gmail.com>
2021-03-08 11:03:01 +0500

    User Authentication form

---
+++ script/app.js
@@ -0,0 +1,54 @@
+(function() {
+    
+    var firebaseConfig = {
+        apiKey: "AIzaSyDz_DnJyVKt-2KNnhTCljs9T9HCJ62pIB8",
+        authDomain: "ninetyplussolutions.firebaseapp.com",
+        projectId: "ninetyplussolutions",
+        storageBucket: "ninetyplussolutions.appspot.com",
+        messagingSenderId: "696084884980",
+        appId: "1:696084884980:web:f8a1a3fc973d0405a51da1"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+    //  Fetching DOM Data
+    const eMail = document.getElementById("userEmail");
+    const userPass = document.getElementById("userPass");
+    const btnLogin = document.getElementById("login");
+    const btnSignUp = document.getElementById("signUp");
+    const btnLogOut = document.getElementById("logout");
+    // let Users Login
+    btnLogin.addEventListener('click', e =>{
+        const userEmail = eMail.value;
+        const pass = userPass.value;
+        const authentication = firebase.auth();
+        // Sign in
+       const details = authentication.signInWithEmailAndPassword(userEmail, pass);
+       details.catch(e => alert("User Doesnt Exist Please Signup to Continue"));
+ 
+    });
+
+    // SignUp Function
+    btnSignUp.addEventListener('click', e =>{
+
+        const userEmail = eMail.value;
+        const pass = userPass.value;
+        const authentication = firebase.auth();
+        // Create a new User
+       const details = authentication.createUserWithEmailAndPassword(userEmail, pass);
+       details.catch(e => console.log(e.message));
+
+    });
+    // Real User Validation
+    firebase.auth().onAuthStateChanged(firebaseUser =>{
+        if (firebaseUser) {
+            
+            console.log(firebaseUser);
+        }
+        else{
+            console.log("Not Logged In");
+        }
+
+    });
+
+}());
